d750da1a001be4a0082d0a72e48912c7ec6f60bc
Eddie Yantis <eyantis@csumb.edu>
2016-09-20 18:27:17 +0000

    updated card functions

--- card.php
+++ card.php
@@ -1,19 +1,30 @@
 <?php
 class Card
 {
-    public function __construct($v)
+    public function __construct($v, $s)
     {
-        $this->value = $v; 
+        $this->value = $v;
+        $this->suit = $s;
     }
     function test() {
-        echo "$value <br>";
+        echo "$this->value of $this->suit <br>";
     }
 }
 
+$suits = array("clubs", "diamonds", "hearts", "spades");
 $card = array();
-for($i = 0; $i < 52; $i++) {
-    $card[$i] = new Card($i);
+
+for($i = 0; $i < 4; $i++) {
+    for($j = 0; $j < 13; $j++) {
+        array_push($card, new Card($j, $suits[$i]));
+    }
+        
 }
+
 shuffle($card);
+for($i = 0; $i < 52; $i++) {
+    $card[$i] -> test();
+}
+
 
 ?>
